fe238f77750dfa56458bcbd7ec1e17cb50cb63b0
Yisacc Aberham <yaberham@blackboxintelligence.com>
2022-03-26 18:59:35 +0300

    Projects page created and integrated with the api

---
+++ apps/bug-tracker/src/pages/projects/projects.tsx
@@ -0,0 +1,26 @@
+import { Card, Col, Row } from 'antd';
+
+ const ProjectPage=()=>{
+  return(
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+export default ProjectPage
